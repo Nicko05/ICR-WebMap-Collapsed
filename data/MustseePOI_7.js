32607d73e5c3e7b166cbeabc911c3c9645d0ef9e
Nicko05 <55138611+Nicko05@users.noreply.github.com>
2023-09-01 09:32:03 +0200

    Add files via upload

---
+++ data/MustseePOI_7.js
@@ -0,0 +1,19 @@
+var json_MustseePOI_7 = {
+"type": "FeatureCollection",
+"name": "MustseePOI_7",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Opis/Description": "Ivanečki bajeri s sjedištem ŠRD", "Koordinate/Coordinates": "46.23009027, 16.11135116", "Nadmorska visina/Altitude": "210" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ 16.111348479159226, 46.230090837679825 ] ] } },
+{ "type": "Feature", "properties": { "Opis/Description": "Keramika Mecena", "Koordinate/Coordinates": "46.23821463, 16.10072121", "Nadmorska visina/Altitude": "235" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ 16.100718941420638, 46.238212919021827 ] ] } },
+{ "type": "Feature", "properties": { "Opis/Description": "Panoramski pogled Gačice", "Koordinate/Coordinates": "46.22897672, 16.19862187", "Nadmorska visina/Altitude": "291" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ 16.198620839561382, 46.228977007016312 ] ] } },
+{ "type": "Feature", "properties": { "Opis/Description": "Muzej Planinarstva", "Koordinate/Coordinates": "46.22158337, 16.12548184", "Nadmorska visina/Altitude": "230" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ 16.125480399435038, 46.221583087656342 ] ] } },
+{ "type": "Feature", "properties": { "Opis/Description": "Friščićev Mlin", "Koordinate/Coordinates": "46.20049138, 16.13414309", "Nadmorska visina/Altitude": "372" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ 16.134141232940443, 46.200491948020868 ] ] } },
+{ "type": "Feature", "properties": { "Opis/Description": "Izletište Jarki", "Koordinate/Coordinates": "46.19435970, 16.14493957", "Nadmorska visina/Altitude": "440" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ 16.1449430787243, 46.194358841031772 ] ] } },
+{ "type": "Feature", "properties": { "Opis/Description": "Vodenica u Margečanu", "Koordinate/Coordinates": "46.21482855, 16.22741937", "Nadmorska visina/Altitude": "195" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ 16.227418337132814, 46.214827410346132 ] ] } },
+{ "type": "Feature", "properties": { "Opis/Description": "Kapela sveti Duh u Prigorcu", "Koordinate/Coordinates": "46.20591834, 16.13813876", "Nadmorska visina/Altitude": "449" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ 16.138135255430864, 46.205919200059952 ] ] } },
+{ "type": "Feature", "properties": { "Opis/Description": "Lovački dom „Šumski zec“", "Koordinate/Coordinates": "46.22292763, 16.20657359", "Nadmorska visina/Altitude": "214" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ 16.20657420392379, 46.222927061644228 ] ] } },
+{ "type": "Feature", "properties": { "Opis/Description": "Panoramski pogled s parkinga Repikovac", "Koordinate/Coordinates": "46.19696219, 16.13879552", "Nadmorska visina/Altitude": "437" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ 16.138792834696108, 46.196963615587002 ] ] } },
+{ "type": "Feature", "properties": { "Opis/Description": "Bedenska hiža", "Koordinate/Coordinates": "46.23880966, 16.06301262", "Nadmorska visina/Altitude": "243" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ 16.063011176713687, 46.238809664229521 ] ] } },
+{ "type": "Feature", "properties": { "Opis/Description": "Salinovec-kod stare škole (dolina rijeke BEdnje + drveni most)", "Koordinate/Coordinates": "46.23005686, 16.16645017", "Nadmorska visina/Altitude": "207" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ 16.166450376792518, 46.230056 ] ] } }
+]
+}
